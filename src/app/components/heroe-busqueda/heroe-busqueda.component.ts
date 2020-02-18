a7d3fc2c63eecfa4db7adccdb4296dec447cdc27
JesusSP <jesus.saints@gmail.com>
2020-02-18 13:10:44 +0100

    App done

---
+++ src/app/components/heroe-busqueda/heroe-busqueda.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { ActivatedRoute } from "@angular/router";
+
+
+@Component({
+  selector: 'app-heroe-busqueda',
+  templateUrl: './heroe-busqueda.component.html',
+})
+export class HeroeBusquedaComponent implements OnInit {
+
+  heroes:Heroe[] = []
+  termino:string;
+
+  constructor(private _heroesService:HeroesService,
+              private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params["termino"]);
+      console.log(this.heroes)
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
